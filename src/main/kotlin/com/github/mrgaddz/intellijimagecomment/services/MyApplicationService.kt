87da4e0bad932f28800b9cafdc711f34948ebae1
GitHub Action <action@github.com>
2021-02-11 21:11:57 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/mrgaddz/intellijimagecomment/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mrgaddz.intellijimagecomment.services
+
+import com.github.mrgaddz.intellijimagecomment.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/mrgaddz/intellijimagecomment/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mrgaddz/intellijimagecomment/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mrgaddz.intellijimagecomment.services
 
+import com.github.mrgaddz.intellijimagecomment.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
